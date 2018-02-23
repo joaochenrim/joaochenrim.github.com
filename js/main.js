61d5eaedce07ddb031414a240a7aed0bd2e62eac
João Chenrim <joaopcchenrim@gmail.com>
2018-02-23 12:58:28 +0000

    23-2

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ var header = document.getElementById('header');
 window.onscroll = function() {
     if (window.pageYOffset > 300) {
         header.classList.add("header--dark");
+    
     } else {
         header.classList.remove("header--dark");
     }
-}
+};
 		
-console.log("working");
 
